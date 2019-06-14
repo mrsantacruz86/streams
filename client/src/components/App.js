ecd7e270591ddcff2458b0c21b748834accf6e98
Nelson Diaz <abnerdc86@gmail.com>
2019-06-13 22:38:16 -0400

    Setup pages and routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-
-const PageOne = () => <div>PageOne</div>;
-const PageTwo = () => (
-  <div>
-    PageTwo
-    <button class="btn">Click Me!</button>
-  </div>
-);
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
