7008a045af8e785eb8378c3603e8e39c2e72bb9f
Nelson Diaz <abnerdc86@gmail.com>
2019-06-17 13:33:04 -0400

    Select a stream to edit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -36,7 +36,7 @@ export const fetchStreams = () => async dispatch => {
 };
 
 export const fetchStream = id => async dispatch => {
-  const response = await streams.get(`/streams${id}`);
+  const response = await streams.get(`/streams/${id}`);
   dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
@@ -46,6 +46,6 @@ export const editStreams = (id, formValues) => async dispatch => {
 };
 
 export const deleteStream = id => async dispatch => {
-  const response = await streams.delete(`/streams${id}`);
+  await streams.delete(`/streams${id}`);
   dispatch({ type: DELETE_STREAM, payload: id });
 };
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
 class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
   render() {
-    console.log(this.props);
-    return <div>StreamEdit</div>;
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
   }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamEdit);
