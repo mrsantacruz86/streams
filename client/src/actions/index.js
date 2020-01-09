624ad0f2f383496f21f06a41ff7bb78a81e5fdff
Nelson Diaz <abnerdc86@gmail.com>
2020-01-09 16:46:59 -0500

    fixing action to allow for firebase firestore

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
-import streams from "../apis/streams";
-import { db } from "../apis/fbStreams";
-import history from "../history";
+import streams from '../apis/streams';
+import { db } from '../apis/fbStreams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -9,7 +9,7 @@ import {
   FETCH_STREAMS,
   EDIT_STREAM,
   DELETE_STREAM
-} from "./types";
+} from './types';
 
 export const signIn = userId => {
   return {
@@ -24,35 +24,54 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async (dispatch, getState) => {
+export const createStream = formValues => (dispatch, getState) => {
   const { userId } = getState().auth;
-  console.log(userId);
-  // const response = await streams.post("/streams", { ...formValues, userId });
-  const response = await db
-    .collection("streams")
-    .add({ ...formValues, userId });
-  dispatch({ type: CREATE_STREAM, payload: response.data });
-  history.push("/");
+  // console.log(userId);
+  db.collection('streams')
+    .add({ ...formValues, userId })
+    .then(docRef => {
+      console.log('Stream Added! ID: ', docRef.id);
+      history.push('/');
+    })
+    .catch(error => {
+      console.error('Error adding document: ', error);
+    });
 };
 
-export const fetchStreams = () => async dispatch => {
-  const response = await streams.get("/streams");
-  dispatch({ type: FETCH_STREAMS, payload: response.data });
+export const fetchStreams = () => dispatch => {
+  db.collection('streams').onSnapshot(snapshot => {
+    let streams = [];
+    snapshot.forEach(doc => {
+      streams.push({ ...doc.data(), id: doc.id });
+    });
+    dispatch({ type: FETCH_STREAMS, payload: streams });
+  });
 };
 
-export const fetchStream = id => async dispatch => {
-  const response = await streams.get(`/streams/${id}`);
-  dispatch({ type: FETCH_STREAM, payload: response.data });
+export const fetchStream = id => dispatch => {
+  db.collection('streams')
+    .doc(id)
+    .snapshot.forEach(doc => {
+      dispatch({ type: FETCH_STREAMS, payload: { ...doc.data(), id: doc.id } });
+    });
 };
 
 export const editStream = (id, formValues) => async dispatch => {
   const response = await streams.patch(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
-  history.push("/");
+  history.push('/');
 };
 
-export const deleteStream = id => async dispatch => {
-  await streams.delete(`/streams/${id}`);
-  dispatch({ type: DELETE_STREAM, payload: id });
-  history.push("/");
+export const deleteStream = id => dispatch => {
+  db.collection('streams')
+    .doc(id)
+    .delete()
+    .then(() => {
+      dispatch({ type: DELETE_STREAM, payload: id });
+      history.push('/');
+      console.log('Document successfully deleted!');
+    })
+    .catch(error => {
+      console.error('Error removing document: ', error);
+    });
 };
