485eab5c52e9884adbe255c4704d7adeb2cbb69b
Nelson Diaz <abnerdc86@gmail.com>
2020-01-14 18:25:47 -0500

    working with cloud function for rtmp

--- client/functions/index.js
+++ client/functions/index.js
@@ -3,6 +3,29 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+const NodeMediaServer = require("node-media-server");
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: "*"
+  }
+};
+
+var nms = new NodeMediaServer(config);
+nms.run();
+
+exports.mediaServer = functions.https.onRequest((request, response) => {
+    response.send(nms)
+})
